1e3c4bd48b60e3b400b48a6e1067b35ac4ac2358
Euan Hunter <samlex@gmx.com>
2015-05-09 10:47:37 +0000

    Added skeleton database classes

---
+++ src/SamLex/SWCProspect/Database/DatabaseInteractor.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace SamLex\SWCProspect\Database;
+
+interface DatabaseInteractor
+{}
---
+++ src/SamLex/SWCProspect/Database/MySQLDatabaseInteractor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SamLex\SWCProspect\Database;
+
+use SamLex\SWCProspect\Database\DatabaseInteractor;
+
+class MySQLDatabaseInteractor implements DatabaseInteractor
+{}
