dbb7321bd66d3731f38b060758680506d9fd42c4
Euan Hunter <samlex@gmx.com>
2015-05-09 13:43:48 +0000

    Created data constructors
    Created constructs for Planet and Result class

--- src/SamLex/SWCProspect/Planet.php
+++ src/SamLex/SWCProspect/Planet.php
@@ -3,4 +3,18 @@
 namespace SamLex\SWCProspect;
 
 class Planet
-{}
+{
+    private $dbID;
+    private $name;
+    private $size;
+
+    private $source;
+
+    public function __construct($src, $name, $size, $id = -1)
+    {
+        $this->source = $src;
+        $this->name = $name;
+        $this->size = $size;
+        $this->dbID = $id;
+    }
+}
--- src/SamLex/SWCProspect/Result.php
+++ src/SamLex/SWCProspect/Result.php
@@ -3,4 +3,24 @@
 namespace SamLex\SWCProspect;
 
 class Result
-{}
+{
+    private $dbID;
+    private $planetID;
+    private $material;
+    private $size;
+    private $locationX;
+    private $locationY;
+
+    private $source;
+
+    public function __construct($src, $planetID, $material, $size, $locX, $locY, $id = -1)
+    {
+        $this->source = $src;
+        $this->planetID = $planetID;
+        $this->material = $material;
+        $this->size = $size;
+        $this->locationX = $locX;
+        $this->locationY = $locY;
+        $this->dbID = $id;
+    }
+}
