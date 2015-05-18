3291cea046bac77db8c086fa29a1b044ae1119c1
Euan Hunter <samlex@gmx.com>
2015-05-18 15:18:46 +0000

    Implemented delete planet worker

--- web/deleteplanetworker.php
+++ web/deleteplanetworker.php
@@ -13,6 +13,18 @@ use SamLex\SWCProspect\Database\MySQLDatabaseInteractor as MySQL;
 // Connect to database
 $db = new MySQL($CONFIG['database_address'], $CONFIG['database_user'], $CONFIG['database_password'], $CONFIG['database_name']);
 
+// Sanitize and validate GET parameters
+$cleanPlanetID = filter_input(INPUT_GET, 'planetid', FILTER_SANITIZE_NUMBER_INT);
+$validPlanetID = filter_var($cleanPlanetID, FILTER_VALIDATE_INT);
+
+if ($validPlanetID) {
+    $planet = $db->getPlanet($validPlanetID);
+
+    if ($planet) {
+        $planet->delete();
+    }
+}
+
 // Redirect back to main page
 header('HTTP/1.1 301 Moved Permanently');
 header('Location: '.'index.php');
