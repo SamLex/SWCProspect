1100dc5e072f91f90b42099d39f0dbe6337ddece
Euan Hunter <samlex@gmx.com>
2015-05-19 09:04:02 +0000

    Added needed parameters to edit pages

--- web/editdeposit.php
+++ web/editdeposit.php
@@ -14,6 +14,16 @@ use SamLex\SWCProspect\Database\MySQLDatabaseInteractor as MySQL;
 // Connect to database
 $db = new MySQL($CONFIG['database_address'], $CONFIG['database_user'], $CONFIG['database_password'], $CONFIG['database_name']);
 
+// Sanitize and validate GET parameters
+$cleanDepositID = filter_input(INPUT_GET, 'depositid', FILTER_SANITIZE_NUMBER_INT);
+$validDepositID = filter_var($cleanDepositID, FILTER_VALIDATE_INT);
+
+if ($validDepositID) {
+    $depositID = $validDepositID;
+} else {
+    $depositID = -1;
+}
+
 // Create page and output
-$page = new EditDepositPage($db);
+$page = new EditDepositPage($db, $depositID);
 $page->outputPage();
