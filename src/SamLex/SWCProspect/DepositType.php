bfdc3a122925b59b22155f3064523d6a5d67dd4d
Euan Hunter <samlex@gmx.com>
2015-05-25 10:21:05 +0000

    Added PHPdoc comments to DepositType and PlanetType classes

--- src/SamLex/SWCProspect/DepositType.php
+++ src/SamLex/SWCProspect/DepositType.php
@@ -1,16 +1,39 @@
 <?php
 
+/** Part of SWCProspect, contains DepositType class. */
 namespace SamLex\SWCProspect;
 
-/*
-    Data class that holds data on a deposit type
-*/
+/** Data class that holds data on a deposit type. */
 class DepositType
 {
+    /**
+     * The database ID of the deposit type.
+     *
+     * @var int
+     */
     private $dbID;
+
+    /**
+     * The material name of the deposit type.
+     *
+     * @var string
+     */
     private $material;
+
+    /**
+     * The HTML colour to be used to represent this deposit type.
+     *
+     * @var string
+     */
     private $htmlColour;
 
+    /**
+     * Constructs a new DepositType instance.
+     *
+     * @param int    $dbID       The database ID of the deposit type.
+     * @param string $material   The material name of the deposit type.
+     * @param string $htmlColour The HTML colour to be used to represent this deposit type.
+     */
     public function __construct($dbID, $material, $htmlColour)
     {
         $this->dbID = $dbID;
@@ -18,16 +41,31 @@ class DepositType
         $this->htmlColour = $htmlColour;
     }
 
+    /**
+     * Returns the database ID for this type.
+     *
+     * @return int
+     */
     public function getDBID()
     {
         return $this->dbID;
     }
 
+    /**
+     * Returns the material name for this type.
+     *
+     * @return string
+     */
     public function getMaterial()
     {
         return $this->material;
     }
 
+    /**
+     * Returns the HTML colour for this type.
+     *
+     * @return string
+     */
     public function getHTMLColour()
     {
         return $this->htmlColour;
--- src/SamLex/SWCProspect/PlanetType.php
+++ src/SamLex/SWCProspect/PlanetType.php
@@ -1,16 +1,39 @@
 <?php
 
+/** Part of SWCProspect, contains PlanetType class. */
 namespace SamLex\SWCProspect;
 
-/*
-    Data class that holds data on a planet type
-*/
+/** Data class that holds data on a planet type. */
 class PlanetType
 {
+    /**
+     * The database ID of the planet type.
+     *
+     * @var int
+     */
     private $dbID;
+
+    /**
+     * The description of the planet type.
+     *
+     * @var string
+     */
     private $description;
+
+    /**
+     * The HTML colour to be used to represent this planet type.
+     *
+     * @var string
+     */
     private $htmlColour;
 
+    /**
+     * Constructs a new PlanetType instance.
+     *
+     * @param int    $dbID        The database ID of the planet type.
+     * @param string $description The description of the planet type.
+     * @param string $htmlColour  The HTML colour to be used to represent this planet type.
+     */
     public function __construct($dbID, $description, $htmlColour)
     {
         $this->dbID = $dbID;
@@ -18,16 +41,31 @@ class PlanetType
         $this->htmlColour = $htmlColour;
     }
 
+    /**
+     * Returns the database ID for this type.
+     *
+     * @return int
+     */
     public function getDBID()
     {
         return $this->dbID;
     }
 
+    /**
+     * Returns the description for this type.
+     *
+     * @return string
+     */
     public function getDescription()
     {
         return $this->description;
     }
 
+    /**
+     * Returns the HTML colour for this type.
+     *
+     * @return string
+     */
     public function getHTMLColour()
     {
         return $this->htmlColour;
