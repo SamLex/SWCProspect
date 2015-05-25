4db5b39871cce18f0be3ac031c05465e491d29f6
Euan Hunter <samlex@gmx.com>
2015-05-25 10:07:40 +0000

    Added PHPDoc comments and new init method to ViewPlanetPage class

--- src/SamLex/SWCProspect/Page/ViewPlanetPage.php
+++ src/SamLex/SWCProspect/Page/ViewPlanetPage.php
@@ -1,12 +1,22 @@
 <?php
 
+/** Part of SWCProspect, contains ViewPlanetPage class. */
 namespace SamLex\SWCProspect\Page;
 
-/*
-    The view planet page
-*/
+use SamLex\SWCProspect\Database\DatabaseInteractor;
+use SamLex\SWCProspect\Planet;
+use SamLex\SWCProspect\Deposit;
+
+/**
+ * The view planet page.
+ *
+ * Shows basic information about the planet, a grid showing deposit locations with associated popups
+ * and a table showing deposit information with relevant edit and delete buttons. Also includes a
+ * planet control (add deposit, edit planet, delete planet) menu.
+ */
 class ViewPlanetPage extends Page
 {
+    /** The template for the planet control menu. */
     private $menuPopupTemplate = "
     <div data-role='popup' id='%%PLANET_NAMEID%%MenuPopup'>
         <ul data-role='listview' data-inset='true'>
@@ -18,6 +28,7 @@ class ViewPlanetPage extends Page
     </div>
     ";
 
+    /** The template for the planet delete popup. */
     private $deletePopupTemplate = "
     <div data-role='popup' id='%%PLANET_NAMEID%%DeletePopup' data-overlay-theme='b'>
         <div data-role='header'>
@@ -32,6 +43,7 @@ class ViewPlanetPage extends Page
     </div>
     ";
 
+    /** The template for the deposit delete popup. */
     private $depositDeletePopupTemplate = "
     <div data-role='popup' id='%%DEPOSIT_ID%%DepositDeletePopup' data-overlay-theme='b'>
         <div data-role='header'>
@@ -46,6 +58,7 @@ class ViewPlanetPage extends Page
     </div>
     ";
 
+    /** The template for the deposit tooltip popup used in the grid. */
     private $depositTooltipPopupTemplate = "
     <div data-role='popup' id='%%DEPOSIT_ID%%(%%DEPOSIT_COORD%%)TooltipPopup' class='ui-content'>
         <p>%%DEPOSIT_SIZE%% %%DEPOSIT_MAT%%</p>
@@ -53,6 +66,7 @@ class ViewPlanetPage extends Page
     </dv>
     ";
 
+    /** The template for the basic planet info. */
     private $infoGridTemplate = "
     <div class='ui-grid-a ui-responsive'>
         <div class='ui-block-a'>
@@ -88,6 +102,7 @@ class ViewPlanetPage extends Page
     </div>
     ";
 
+    /** The template for a deposit grid cell that has a deposit. */
     private $depositGridFullCellTemplate = "
     <td class='view-planet-page-deposit-grid-cell' style='background-color:%%DEPOSIT_COLOUR%%'>
         <a href='#%%DEPOSIT_ID%%(%%DEPOSIT_COORD%%)TooltipPopup' style='display:block;width:100%;height:100%' title='(%%DEPOSIT_COORD%%)' data-rel='popup' data-transition='pop'>
@@ -95,11 +110,13 @@ class ViewPlanetPage extends Page
     </td>
     ";
 
+    /** The template for a deposit grid cell that is empty. */
     private $depositGridEmptyCellTemplate = "
     <td class='view-planet-page-deposit-grid-cell'>
     </td>
     ";
 
+    /** The template for the deposit table. */
     private $depositTableTemplate = "
     <table data-role='table' data-mode='reflow' class='ui-body-b ui-shadow ui-responsive table-stripe'>
         <thead>
@@ -117,6 +134,7 @@ class ViewPlanetPage extends Page
     </table>
     ";
 
+    /** The template for a row in the deposit table. */
     private $depositTableCellTemplate = "
     <tr>
         <td style='vertical-align:middle'>%%DEPOSIT_MAT%%</td>
@@ -127,64 +145,103 @@ class ViewPlanetPage extends Page
     </tr>
     ";
 
+    /** The format (sprintf) used to create the menu button. */
+    private $menuButtonFormat = "
+    <a data-icon='gear' data-iconpos='notext' class='ui-btn-right' href='#%s%dMenuPopup' data-rel='popup'></a>
+    ";
+
+    /** The DatabaseInteractor instance to use to get data. */
+    private $db = null;
+
+    /** The planet id of the planet being viewed. */
+    private $planetID = -1;
+
+    /**
+     * Constructs a new MainPage instance.
+     *
+     * @param DatabaseInteractor $db       The DatabaseInteractor instance to use to get data.
+     * @param int                $planetID The planet id of the planet being viewed.
+     */
     public function __construct($db, $planetID)
     {
-        $dbError = !$db->isAvailable();
+        $this->db = $db;
+        $this->planetID = $planetID;
+        $this->addToJQHeaderBeforeTitle($this->backButtonTemplate);
+    }
 
-        if ($dbError === false) {
-            $planet = $db->getPlanet($planetID);
+    /**
+     * Initializes the page.
+     *
+     * @return bool True if the page initialized successfully.
+     */
+    public function init()
+    {
+        $planet = Planet::getPlanet($this->db, $this->planetID);
+        $deposits = $planet->getDeposits();
 
-            if ($planet === false) {
-                $dbError = true;
-            } else {
-                $deposits = $db->getDeposits($planet->getDBID());
+        if (!is_null($planet)) {
+            $spacelessName = str_replace(' ', '', $planet->getName());
 
-                if ($deposits === false) {
-                    $dbError = true;
-                }
-            }
-        }
-
-        $this->addToJQHeaderBeforeTitle("<a data-icon='back' data-iconpos='notext' class='ui-btn-left' data-rel='back'></a>");
-
-        if ($dbError === true) {
-            $this->setJQPageID('swcprospect-view-planet-page-error');
-            $this->setTitle('Error');
-            $this->addToJQContent('<p><b>Database error. Unable to continue.</b></p>');
-        } else {
-            $this->setJQPageID(sprintf('swcprospect-view-planet-page-%s%d', str_replace(' ', '', $planet->getName()), $planet->getDBID()));
+            $this->setJQPageID(sprintf('swcprospect-view-planet-page-%s%d', $spacelessName, $planet->getDBID()));
             $this->setTitle($planet->getName());
-            $this->addToJQHeaderAfterTitle(sprintf("<a data-icon='gear' data-iconpos='notext' class='ui-btn-right' href='#%s%dMenuPopup' data-rel='popup'></a>", str_replace(' ', '', $planet->getName()), $planet->getDBID()));
-            $this->addToJQAfterContent($this->menuPopup($planet));
-            $this->addToJQAfterContent($this->deletePopup($planet));
+            $this->addToJQHeaderAfterTitle(sprintf($this->menuButtonFormat, $spacelessName, $planet->getDBID()));
+            $this->addToJQAfterContent($this->menuPopup($planet, $spacelessName));
+            $this->addToJQAfterContent($this->deletePopup($planet, $spacelessName));
             $this->addToJQAfterContent($this->depositDeletePopups($deposits));
             $this->addToJQAfterContent($this->depositTooltipPopups($deposits));
             $this->addToJQContent($this->infoGrid($planet, $deposits));
             $this->addToJQContent($this->depositsTable($deposits));
+
+            return true;
+        } else {
+            return false;
         }
     }
 
-    private function menuPopup($planet)
+    /**
+     * Generates the planet menu popup.
+     *
+     * @param Planet $planet        The planet.
+     * @param string $spacelessName The name of the planet stripped of spaces.
+     *
+     * @return string The generated popup.
+     */
+    private function menuPopup($planet, $spacelessName)
     {
         $popup = $this->menuPopupTemplate;
 
-        $popup = str_replace('%%PLANET_NAMEID%%', sprintf('%s%d', str_replace(' ', '', $planet->getName()), $planet->getDBID()), $popup);
+        $popup = str_replace('%%PLANET_NAMEID%%', sprintf('%s%d', $spacelessName, $planet->getDBID()), $popup);
         $popup = str_replace('%%PLANET_ID%%', $planet->getDBID(), $popup);
 
         return $popup;
     }
 
-    private function deletePopup($planet)
+    /**
+     * Generates the planet delete popup.
+     *
+     * @param Planet $planet        The planet.
+     * @param string $spacelessName The name of the planet stripped of spaces.
+     *
+     * @return string The generated popup.
+     */
+    private function deletePopup($planet, $spacelessName)
     {
         $popup = $this->deletePopupTemplate;
 
-        $popup = str_replace('%%PLANET_NAMEID%%', sprintf('%s%d', str_replace(' ', '', $planet->getName()), $planet->getDBID()), $popup);
+        $popup = str_replace('%%PLANET_NAMEID%%', sprintf('%s%d', $spacelessName, $planet->getDBID()), $popup);
         $popup = str_replace('%%PLANET_ID%%', $planet->getDBID(), $popup);
         $popup = str_replace('%%PLANET_NAME%%', $planet->getName(), $popup);
 
         return $popup;
     }
 
+    /**
+     * Generates the deposit delete popups.
+     *
+     * @param Deposit[] $deposits The deposits.
+     *
+     * @return string The generated popups.
+     */
     private function depositDeletePopups($deposits)
     {
         $popups = '';
@@ -200,6 +257,13 @@ class ViewPlanetPage extends Page
         return $popups;
     }
 
+    /**
+     * Generates the deposit tooltip popups.
+     *
+     * @param Deposit[] $deposits The deposits.
+     *
+     * @return string The generated popups.
+     */
     private function depositTooltipPopups($deposits)
     {
         $popups = '';
@@ -218,6 +282,14 @@ class ViewPlanetPage extends Page
         return $popups;
     }
 
+    /**
+     * Generates the basic info grid (JQ).
+     *
+     * @param Planet    $planet   The planet.
+     * @param Deposit[] $deposits The deposits.
+     *
+     * @return string The generated JQ grid.
+     */
     private function infoGrid($planet, $deposits)
     {
         $info = $this->infoGridTemplate;
@@ -232,6 +304,14 @@ class ViewPlanetPage extends Page
         return $info;
     }
 
+    /**
+     * Generates the deposit grid.
+     *
+     * @param Deposit[] $deposits   The deposits.
+     * @param int       $planetSize The size of the planet.
+     *
+     * @return string The generated grid.
+     */
     private function depositsGrid($deposits, $planetSize)
     {
         $depositCoordArray = array();
@@ -268,6 +348,13 @@ class ViewPlanetPage extends Page
         return $boxes;
     }
 
+    /**
+     * Generates the deposit table.
+     *
+     * @param Deposit[] $deposits The deposits.
+     *
+     * @return string The generated table.
+     */
     private function depositsTable($deposits)
     {
         $table = $this->depositTableTemplate;
